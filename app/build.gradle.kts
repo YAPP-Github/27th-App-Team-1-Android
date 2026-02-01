1b8dbac3f1dff3b3bd9562a64b90de2a9115cb79
mj010504 <mj010504@naver.com>
2026-02-01 20:50:43 +0900

    [NDGL-49]  feature: TravelMap 컴포넌트 추가 및 Google Map 관련 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     id("ndgl.application")
     alias(libs.plugins.google.services)
@@ -6,6 +8,14 @@ plugins {
 
 android {
     namespace = Configuration.APPLICATION_ID
+
+    defaultConfig {
+        val localProperties = Properties().apply {
+            load(rootProject.file("local.properties").bufferedReader())
+        }
+
+        manifestPlaceholders["MAPS_API_KEY"] = localProperties.getProperty("MAPS_API_KEY", "")
+    }
 
     buildFeatures {
         buildConfig = true
